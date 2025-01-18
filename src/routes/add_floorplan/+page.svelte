1f24debd498752bbdcd82de0975159f04c079873
bedminer1 <alexander.seah.min.rui@gmail.com>
2025-01-18 15:54:48 +0800

    inserting png into frontpage

--- src/routes/add_floorplan/+page.svelte
+++ src/routes/add_floorplan/+page.svelte
@@ -8,19 +8,19 @@
   </nav>
   <div class="flex flex-col items-center p-10">
       <h2 class="text-4xl mb-10">ADD FLOORPLAN</h2>
-      <form method="post" class="flex flex-col gap-5 w-1/2 text-xl items-center p-5 mb-10">
+      <form method="post" class="flex flex-col gap-5 w-2/3 text-xl items-center p-5 mb-10">
         <label class="w-full flex justify-between">
           <p class="flex items-center text-xl">
-            Floorplan Name
+            Floor Name
           </p>
-          <input type="text" class="input w-1/2 md:w-2/3" name="asset" bind:value={name} placeholder="e.g., ETF_EMB" required />
+          <input type="text" class="input w-2/3" name="asset" bind:value={name} placeholder="e.g. Block B, Level 5" required />
         </label> 
         
         <label class="w-full flex justify-between mb-4">
         <p class="flex items-center text-xl">
-            .png File
+            Image File
         </p>
-        <input type="file" class="input" bind:value={file}>
+        <input type="file" class="input w-2/3 text-sm text-gray-400" bind:value={file}>
         </label>
         
           <button class="btn  text-lg variant-ghost-primary" type="submit">Submit</button>
