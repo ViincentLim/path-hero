41fb3c95aec104c74aef9d3ee15a1e68349e1ac3
bedminer1 <alexander.seah.min.rui@gmail.com>
2025-01-18 15:04:32 +0800

    added floorplan api fetch and receive

---
+++ src/routes/add_floorplan/+page.server.ts
@@ -0,0 +1,29 @@
+import { saveResponseToFile } from '$lib/utils/saveResponse'
+import { redirect } from '@sveltejs/kit';
+
+export const actions = {
+  default: async ({ request }) => {
+    const formData = await request.formData();
+    const name = formData.get("name");
+    const file = formData.get("file");
+
+    // Validate inputs
+    if (!name || !file ) {
+      return { error: "All fields are required." };
+    }
+
+    const apiUrl = `http://localhost:4000/recommendations?name=${name}&file=${file}`
+
+    // Fetch data from the backend
+    const response = await fetch(apiUrl);
+
+    if (!response.ok) {
+      throw new Error(`API request failed with status ${response.status}`);
+    }
+
+    const data = await response.json()
+    await saveResponseToFile(data, "recommendations.json")
+
+    redirect(303, "/")
+  },
+}
--- src/routes/add_floorplan/+page.svelte
+++ src/routes/add_floorplan/+page.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
-    let start = "2016-01-01";
-      let end = "2017-01-01";
-      let asset = "";
-      let time_intervals = 50;
-      let time_interval_length = "86400";
+    let name: string
+    let file: string
   </script>
   
   <nav class="flex p-4 gap-4 underline">
@@ -14,16 +11,16 @@
       <form method="post" class="flex flex-col gap-5 w-1/2 text-xl items-center p-5 mb-10">
         <label class="w-full flex justify-between">
           <p class="flex items-center text-xl">
-              Floorplan Name
+            Floorplan Name
           </p>
-          <input type="text" class="input w-1/2 md:w-2/3" name="asset" bind:value={asset} placeholder="e.g., ETF_EMB" required />
+          <input type="text" class="input w-1/2 md:w-2/3" name="asset" bind:value={name} placeholder="e.g., ETF_EMB" required />
         </label> 
         
         <label class="w-full flex justify-between mb-4">
         <p class="flex items-center text-xl">
-            Time Interval
+            .png File
         </p>
-        <input type="file" class="input">
+        <input type="file" class="input" bind:value={file}>
         </label>
         
           <button class="btn  text-lg variant-ghost-primary" type="submit">Submit</button>
