d6f6dcc902dbad024d86a79da15fe7fc9f7923fb
bedminer1 <alexander.seah.min.rui@gmail.com>
2025-02-18 22:22:42 +0800

    added folder for vip demo

---
+++ demo/src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import '../app.css'
+	import { ModeWatcher, toggleMode } from 'mode-watcher'
+	import { Toaster } from "$lib/components/ui/sonner";
+  	import Switch from '$lib/components/ui/switch/switch.svelte';
+	let { children } = $props()
+
+	let toggled = $state(false)
+
+	$effect(() => {
+		if (toggled) {}
+		toggleMode()
+	})
+	toggleMode()
+</script>
+
+<Toaster />
+<ModeWatcher />
+<div class="p-2 w-full justify-end flex">
+	<Switch bind:checked={toggled}/>
+</div>
+{@render children()}
