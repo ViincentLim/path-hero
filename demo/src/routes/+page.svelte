e549eb8cff96b2a285f01eb92d07bd008c56c89a
bedminer1 <alexander.seah.min.rui@gmail.com>
2025-02-22 22:36:29 +0800

    edited tab names

--- demo/src/routes/+page.svelte
+++ demo/src/routes/+page.svelte
@@ -18,60 +18,83 @@
         </div>
     </Resizable.Pane>
     <Resizable.Handle />
-    <Resizable.Pane defaultSize={25} class="flex justify-center pt-3">
-
+    <Resizable.Pane defaultSize={25} class="flex justify-center p-3">
         <Tabs.Root value="account" class="w-[400px]">
-  <Tabs.List class="grid w-full grid-cols-2">
-    <Tabs.Trigger value="account">Account</Tabs.Trigger>
-    <Tabs.Trigger value="password">Password</Tabs.Trigger>
-  </Tabs.List>
-  <Tabs.Content value="account">
-    <Card.Root>
-      <Card.Header>
-        <Card.Title>Account</Card.Title>
-        <Card.Description>
-          Make changes to your account here. Click save when you're done.
-        </Card.Description>
-      </Card.Header>
-      <Card.Content class="space-y-2">
-        <div class="space-y-1">
-          <Label for="name">Name</Label>
-          <Input id="name" value="Pedro Duarte" />
-        </div>
-        <div class="space-y-1">
-          <Label for="username">Username</Label>
-          <Input id="username" value="@peduarte" />
-        </div>
-      </Card.Content>
-      <Card.Footer>
-        <Button>Save changes</Button>
-      </Card.Footer>
-    </Card.Root>
-  </Tabs.Content>
-  <Tabs.Content value="password">
-    <Card.Root>
-      <Card.Header>
-        <Card.Title>Password</Card.Title>
-        <Card.Description>
-          Change your password here. After saving, you'll be logged out.
-        </Card.Description>
-      </Card.Header>
-      <Card.Content class="space-y-2">
-        <div class="space-y-1">
-          <Label for="current">Current password</Label>
-          <Input id="current" type="password" />
-        </div>
-        <div class="space-y-1">
-          <Label for="new">New password</Label>
-          <Input id="new" type="password" />
-        </div>
-      </Card.Content>
-      <Card.Footer>
-        <Button>Save password</Button>
-      </Card.Footer>
-    </Card.Root>
-  </Tabs.Content>
-</Tabs.Root>
+        <Tabs.List class="grid w-full grid-cols-3">
+          <Tabs.Trigger value="edit">Edit</Tabs.Trigger>
+          <Tabs.Trigger value="instructions">Instructions</Tabs.Trigger>
+          <Tabs.Trigger value="monitor">Monitor</Tabs.Trigger>
+        </Tabs.List>
+        <Tabs.Content value="edit">
+          <Card.Root>
+            <Card.Header>
+              <Card.Title>Edit</Card.Title>
+              <Card.Description>
+                Make changes to your account here. Click save when you're done.
+              </Card.Description>
+            </Card.Header>
+            <Card.Content class="space-y-2">
+              <div class="space-y-1">
+                <Label for="name">Name</Label>
+                <Input id="name" value="Pedro Duarte" />
+              </div>
+              <div class="space-y-1">
+                <Label for="username">Username</Label>
+                <Input id="username" value="@peduarte" />
+              </div>
+            </Card.Content>
+            <Card.Footer>
+              <Button>Save changes</Button>
+            </Card.Footer>
+          </Card.Root>
+        </Tabs.Content>
+        <Tabs.Content value="instructions">
+          <Card.Root>
+            <Card.Header>
+              <Card.Title>Instructions</Card.Title>
+              <Card.Description>
+                Change your password here. After saving, you'll be logged out.
+              </Card.Description>
+            </Card.Header>
+            <Card.Content class="space-y-2">
+              <div class="space-y-1">
+                <Label for="current">Current password</Label>
+                <Input id="current" type="password" />
+              </div>
+              <div class="space-y-1">
+                <Label for="new">New password</Label>
+                <Input id="new" type="password" />
+              </div>
+            </Card.Content>
+            <Card.Footer>
+              <Button>Save password</Button>
+            </Card.Footer>
+          </Card.Root>
+        </Tabs.Content>
+        <Tabs.Content value="monitor">
+          <Card.Root>
+            <Card.Header>
+              <Card.Title>Monitor</Card.Title>
+              <Card.Description>
+                Change your password here. After saving, you'll be logged out.
+              </Card.Description>
+            </Card.Header>
+            <Card.Content class="space-y-2">
+              <div class="space-y-1">
+                <Label for="current">Current password</Label>
+                <Input id="current" type="password" />
+              </div>
+              <div class="space-y-1">
+                <Label for="new">New password</Label>
+                <Input id="new" type="password" />
+              </div>
+            </Card.Content>
+            <Card.Footer>
+              <Button>Save password</Button>
+            </Card.Footer>
+          </Card.Root>
+        </Tabs.Content>
+      </Tabs.Root>
     </Resizable.Pane>
     </Resizable.PaneGroup>
       
